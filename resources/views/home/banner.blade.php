2fa5dbe8066f2decaa3dd9b467827500bb361ee1
Gia Hai <haidao549@gmail.com>
2023-10-25 20:03:47 +0700

    adding styles for banner

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,1 +1,9 @@
-<p>This is a banner</p>
+<div class="banner-container">
+    <img src="{{url('/assets/banner.png')}}" alt="Banner Image"/>
+    <div class="dots-container">
+        <div class="dot active"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+    </div>
+</div>
