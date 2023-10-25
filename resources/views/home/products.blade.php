eeb2ab2d69e0615a2fe4c952309f7b23cb668222
Bao Chau <chauchau0082@gmail.com>
2023-10-25 23:38:08 +0700

    added products grid

--- resources/views/home/products.blade.php
+++ resources/views/home/products.blade.php
@@ -1,1 +1,58 @@
-<p>This is a products grid</p>
+<div class="products-container">
+    <h1>Hôm nay ăn gì ?</h1>
+    <div class="red-line"></div>
+    <div class="products-list row">
+        <div class="product-card col-4">
+            <img src="{{ url('/assets/FoodImg.png') }}" alt="">
+            <p class="product-title">PIZZA SIÊU TOPPING HẢI SẢN XỐT MAYONNAISE - SUPER TOPPING OCEAN MANIA</p>
+            <p class="product-size">Medium</p>
+            <div class="price-container">
+                <p class="product-price">235,000 <u>đ</u></p>
+                <div class="add-icon-container">
+                    <svg width="26" height="26" viewBox="0 0 26 26" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13.2934 4.19995L13.2934 21.8" stroke="white" stroke-width="7"
+                            stroke-linecap="round" />
+                        <path d="M21.8 13.2932L4.19999 13.2932" stroke="white" stroke-width="7"
+                            stroke-linecap="round" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+        <div class="product-card col-4">
+            <img src="{{ url('/assets/FoodImg.png') }}" alt="">
+            <p class="product-title">PIZZA SIÊU TOPPING HẢI SẢN XỐT MAYONNAISE - SUPER TOPPING OCEAN MANIA</p>
+            <p class="product-size">Medium</p>
+            <div class="price-container">
+                <p class="product-price">235,000 <u>đ</u></p>
+                <div class="add-icon-container">
+                    <svg width="26" height="26" viewBox="0 0 26 26" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13.2934 4.19995L13.2934 21.8" stroke="white" stroke-width="7"
+                            stroke-linecap="round" />
+                        <path d="M21.8 13.2932L4.19999 13.2932" stroke="white" stroke-width="7"
+                            stroke-linecap="round" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+		<div class="product-card col-4">
+            <img src="{{ url('/assets/FoodImg.png') }}" alt="">
+            <p class="product-title">PIZZA SIÊU TOPPING HẢI SẢN XỐT MAYONNAISE - SUPER TOPPING OCEAN MANIA</p>
+            <p class="product-size">Medium</p>
+            <div class="price-container">
+                <p class="product-price">235,000 <u>đ</u></p>
+                <div class="add-icon-container">
+                    <svg width="26" height="26" viewBox="0 0 26 26" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13.2934 4.19995L13.2934 21.8" stroke="white" stroke-width="7"
+                            stroke-linecap="round" />
+                        <path d="M21.8 13.2932L4.19999 13.2932" stroke="white" stroke-width="7"
+                            stroke-linecap="round" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="" class="see-more">Xem thêm</a>
+</div>
