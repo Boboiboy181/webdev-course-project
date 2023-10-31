2ebcad0940384014dc6ca307dce9da8e6649ba2b
horralnguyen <21520801@gm.uit.edu.vn>
2023-10-31 22:49:58 +0700

    creating booking section

--- resources/views/home/booking.blade.php
+++ resources/views/home/booking.blade.php
@@ -1,1 +1,12 @@
-<p>This is a booking</p>
+<div class="background">
+    <div class="options">
+        <button class="option-btn delivery">Giao hàng tận nơi</button>
+        <button class="option-btn order">Đặt đến lấy</button>
+    </div>
+    <div class="search-container">
+        <input type="text" placeholder="Vui lòng nhập địa chỉ nhận hàng">
+        <button>
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+    </div>
+</div>
