850e8bc13aee2daedf321324f71cac40f787793d
Bao Chau <chauchau0082@gmail.com>
2023-11-09 10:53:48 +0700

    adding menu pizza

--- resources/views/menu/pizza.blade.php
+++ resources/views/menu/pizza.blade.php
@@ -1,10 +1,60 @@
 @extends('menu.menu')
 
 @section('menu-content')
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Pizza</h1>
-            <p>Pizza content goes here.</p>
+    <div class="pizzas-container">
+        <div class="pizza-item">
+            <div class="pizza-img">
+                <img src="{{url('/assets/menu/pizza/Meat_Lovers.png')}}" alt="">
+            </div>
+            <div class="pizza-detail">
+                <p class="pizza-name">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+                <p class="pizza-option"> <span class="size">MEDIUM</span> - <span class="price">195.000 VNĐ</span></p>
+            </div>
+        </div>
+        <div class="pizza-item">
+            <div class="pizza-img">
+                <img src="{{url('/assets/menu/pizza/Meat_Lovers.png')}}" alt="">
+            </div>
+            <div class="pizza-detail">
+                <p class="pizza-name">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+                <p class="pizza-option"> <span class="size">MEDIUM</span> - <span class="price">195.000 VNĐ</span></p>
+            </div>
+        </div>
+        <div class="pizza-item">
+            <div class="pizza-img">
+                <img src="{{url('/assets/menu/pizza/Meat_Lovers.png')}}" alt="">
+            </div>
+            <div class="pizza-detail">
+                <p class="pizza-name">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+                <p class="pizza-option"> <span class="size">MEDIUM</span> - <span class="price">195.000 VNĐ</span></p>
+            </div>
+        </div>
+        <div class="pizza-item">
+            <div class="pizza-img">
+                <img src="{{url('/assets/menu/pizza/Meat_Lovers.png')}}" alt="">
+            </div>
+            <div class="pizza-detail">
+                <p class="pizza-name">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+                <p class="pizza-option"> <span class="size">MEDIUM</span> - <span class="price">195.000 VNĐ</span></p>
+            </div>
+        </div>
+        <div class="pizza-item">
+            <div class="pizza-img">
+                <img src="{{url('/assets/menu/pizza/Meat_Lovers.png')}}" alt="">
+            </div>
+            <div class="pizza-detail">
+                <p class="pizza-name">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+                <p class="pizza-option"> <span class="size">MEDIUM</span> - <span class="price">195.000 VNĐ</span></p>
+            </div>
+        </div>
+        <div class="pizza-item">
+            <div class="pizza-img">
+                <img src="{{url('/assets/menu/pizza/Meat_Lovers.png')}}" alt="">
+            </div>
+            <div class="pizza-detail">
+                <p class="pizza-name">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+                <p class="pizza-option"> <span class="size">MEDIUM</span> - <span class="price">195.000 VNĐ</span></p>
+            </div>
         </div>
     </div>
 @endsection
