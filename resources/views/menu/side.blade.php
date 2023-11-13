b72696c25a58b9b51263fb6d8aefbf232dbf2b5b
Tien Man Ngo Thuy <127042774+ThuyTienMango@users.noreply.github.com>
2023-11-13 18:51:53 +0700

    menu side blade mon phu

--- resources/views/menu/side.blade.php
+++ resources/views/menu/side.blade.php
@@ -1,10 +1,275 @@
 @extends('menu.menu')
 
 @section('menu-content')
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Sides</h1>
-            <p>Sides content goes here.</p>
+<div class="container my-5 py-5">
+<h1 class="menusides-title mb-4">GÀ</h1>
+<div class="row">
+    <div class="col-md-4 pe-5 ps-4">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
         </div>
     </div>
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>   
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>  
+</div>
+<div class="row my-5">
+    <div class="col-md-4 pe-5 ps-4">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>   
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>  
+</div>
+
+<h1 class="menusides-title mb-4">MỲ Ý</h1>
+<div class="row">
+    <div class="col-md-4 pe-5 ps-4">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>   
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>  
+</div>
+<div class="row my-5">
+    <div class="col-md-4 pe-5 ps-4">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>   
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>  
+</div>
+
+<h1 class="menusides-title mb-4">XÚC XÍCH</h1>
+<div class="row">
+    <div class="col-md-4 pe-5 ps-4">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>   
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>  
+</div>
+
+
+</div>
+
+<h1 class="menusides-title mb-4">BÁNH MỲ</h1>
+<div class="row">
+    <div class="col-md-4 pe-5 ps-4">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>   
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>  
+</div>
+<div class="row my-5">
+    <div class="col-md-4 pe-5 ps-4">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>   
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>  
+</div>
+
+<h1 class="menusides-title mt-4 mb-5 ">KHOAI TÂY</h1>
+<div class="row mb-5">
+    <div class="col-md-4 pe-5 ps-4">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>   
+    <div class="col-md-4 pe-5">
+        <div class="row chickens-container p-4">
+            <div class="col-md-5 chicken-img ms-3">
+
+            </div>
+            <div class="col-md-6 chicken-detail ms-4">
+                <p class="chicken-name ">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            </div>
+        </div>
+    </div>  
+</div>
+ 
+</div>
 @endsection
