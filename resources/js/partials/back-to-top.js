f360208d5066795573f2408b7b5683518f81aa15
MinhTu2509 <21521619@gm.uit.edu.vn>
2023-11-25 19:51:08 +0700

    create back-to-top button

--- resources/js/partials/back-to-top.js
+++ resources/js/partials/back-to-top.js
@@ -1,6 +1,12 @@
 import { Button } from "bootstrap"
 
 // TODO: Code your back-to-top button here
-const backToTop = () => {
-    window.backToTop(0,0);
-}
+const backToTop = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100){
+        backToTop.classList.add("active");
+    } else{
+        backToTop.classList.remove("active");
+    }
+})
