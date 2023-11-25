5568cf00f85d9db3a3a6e0a976e54768aee53d39
horralnguyen <21520801@gm.uit.edu.vn>
2023-11-25 21:26:18 +0700

    sticky

--- resources/js/partials/sticky.js
+++ resources/js/partials/sticky.js
@@ -1,4 +1,11 @@
 // TODO: Code your sticky nav-bar here
 const sticky = () => {
-  // code goes here
+  window.addEventListener('scroll', function() {
+    let navbar = document.getElementsByClassName('myheader border-navbar');
+    if (window.scrollY >= 0) {
+      navbar.classList.add('sticky'); // Add the 'sticky' class
+    } else {
+      navbar.classList.remove('sticky'); // Remove the 'sticky' class
+    }
+  });
 }
