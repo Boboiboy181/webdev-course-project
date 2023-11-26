1f9f4f543b22d7ac483d6d4fee1c95535949bef1
Tien Man Ngo Thuy <127042774+ThuyTienMango@users.noreply.github.com>
2023-11-26 22:43:40 +0700

    add carousel banner php

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,9 +1,38 @@
-<div class="banner-container">
-    <img src="{{url('/assets/banner.png')}}" alt="Banner Image"/>
+<div class="mb-5 banner-container d-flex align-items-center justify-content-center">
+    <!-- <img src="{{url('/assets/banner.png')}}" alt="Banner Image"/>
     <div class="dots-container">
         <div class="dot active"></div>
         <div class="dot"></div>
         <div class="dot"></div>
         <div class="dot"></div>
+    </div> -->
+    <div id="carouselExampleIndicators" style=" max-width: 1040px; max-height: 585px" class="carousel slide">
+        <!-- Phần các chỉ số -->
+        <div class=" dots-container carousel-indicators">
+            <button   type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class=" dot active" aria-current="true" aria-label="Slide 1"></button>
+            <button  type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2" class="dot"></button>
+            <button  type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"  class="dot"></button>
+        </div>
+        <!-- Phần bao các slides -->
+        <div class="carousel-inner" style="border-radius: 23px;">
+            <div class="carousel-item active">
+                <img src="{{url('/assets/banner.png')}}"  alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{url('/assets/banner.png')}}" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{url('/assets/banner.png')}}" alt="...">
+            </div>
+        </div>
+        <!-- Phần điều khiển trái phải -->
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 </div>
