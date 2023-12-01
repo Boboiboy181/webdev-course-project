db8b4447f47d85df7f864ea8554694fcf1b8f9cc
MinhTu2509 <21521619@gm.uit.edu.vn>
2023-12-01 12:06:13 +0700

    create right-side of cart page

--- resources/views/checkout/cart/cart-right.blade.php
+++ resources/views/checkout/cart/cart-right.blade.php
@@ -1,1 +1,41 @@
-<p>RIGHT</p>
+<div class="container cart-right">
+    <h1>Order summary: </h1>
+    <div class="container summary p-5">
+        <div class="row">
+            <div class="col-7">
+                <p>Subtotal:</p>
+            </div>
+            <div class="col-5 price">
+                <p>xxx xxx xxx VND</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-7">
+                <p>Delivery fee:</p>
+            </div>
+            <div class="col-5 price">
+                <p>xxx xxx xxx VND</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-7">
+                <p>Discounted: </p>
+            </div>
+            <div class="col-5 price">
+                <p>xxx xxx xxx VND</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row total">
+            <div class="col-7">
+                <p>Total: </p>
+            </div>
+            <div class="col-5 price">
+                <p>xxx xxx xxx VND</p>
+            </div>
+        </div>
+
+    </div>
+</div>
