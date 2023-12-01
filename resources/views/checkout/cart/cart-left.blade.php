48f4352a378951b41f93bd09223a4a5484d8c9ae
MinhTu2509 <21521619@gm.uit.edu.vn>
2023-12-01 12:05:09 +0700

    create left-side of cart page

--- resources/views/checkout/cart/cart-left.blade.php
+++ resources/views/checkout/cart/cart-left.blade.php
@@ -1,1 +1,54 @@
-<p>LEFT</p>
+<h1>Items</h1>
+<div class="container cart-left">
+    <div class="row items">
+        <div class="col-4 product-img">
+            <img src="" alt="">
+        </div>
+        <div class="col-7 product">
+            <p class="name">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            <p class="price">MEDIUM - 195.000 VNĐ</p>
+        </div>
+        <div class="col-1">
+            <button type="button" class="btn-close" aria-label="Close"></button>
+        </div>
+    </div>
+
+    <div class="row items">
+        <div class="col-4 product-img">
+            <img src="" alt="">
+        </div>
+        <div class="col-7 product">
+            <p class="name">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            <p class="price">MEDIUM - 195.000 VNĐ</p>
+        </div>
+        <div class="col-1">
+            <button type="button" class="btn-close" aria-label="Close"></button>
+        </div>
+    </div>
+
+    <div class="row items">
+        <div class="col-4 product-img">
+            <img src="" alt="">
+        </div>
+        <div class="col-7 product">
+            <p class="name">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            <p class="price">MEDIUM - 195.000 VNĐ</p>
+        </div>
+        <div class="col-1">
+            <button type="button" class="btn-close" aria-label="Close"></button>
+        </div>
+    </div>
+
+    <div class="row items">
+        <div class="col-4 product-img">
+            <img src="" alt="">
+        </div>
+        <div class="col-7 product">
+            <p class="name">PIZZA NEW YORK BÒ BEEFSTEAK PHÔ MAI - NEW YORK CHEESESTEAK</p>
+            <p class="price">MEDIUM - 195.000 VNĐ</p>
+        </div>
+        <div class="col-1">
+            <button type="button" class="btn-close" aria-label="Close"></button>
+        </div>
+    </div>
+</div>
