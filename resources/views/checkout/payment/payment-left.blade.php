24a8fcb08f28ea28a05f706ee1766593e7497720
horralnguyen <21520801@gm.uit.edu.vn>
2023-12-06 12:02:16 +0700

    report_issues(1)

---
+++ resources/views/checkout/payment/payment-left.blade.php
@@ -0,0 +1,21 @@
+<div class=address-container>
+    <h1>Address</h1>
+    <div class="addr-detail">
+        <p>Nguyen Van A - xxxx xxx xxx</p>
+        <p>Address here</p>
+    </div>
+    <div class="delivery-opt-container">
+        <h1>Delivery Options</h1>
+        <div class="row-opt">
+            <div class="priority">
+                <div class="radio-btn">
+                    <input id="priority" type="checkbox">
+                </div>
+                <div class="shipping-info">
+                    <p>xx xxx VND</p>
+                    <p>Priority Delivery</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/checkout/payment/payment.blade.php
+++ resources/views/checkout/payment/payment.blade.php
@@ -3,6 +3,7 @@
 @section('checkout-content')
     <div class="row cart-container">
         <div class="col-6 payment-left">
+            @include('checkout.payment.payment-left')
             @include('checkout.cart.cart-left', ['data' => $data])
         </div>
         <div class="col-6 payment-right">
