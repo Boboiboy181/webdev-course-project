3a41505da8cff7a881cc4e2b05ef66683b391855
Đoàn Văn Anh Hiển <21520227@gm.uit.edu.vn>
2023-12-07 10:22:12 +0700

    refactor: Sync Coding

--- resources/views/checkout/cart/cart-left.blade.php
+++
@@ -1,4 +0,0 @@
-<a class="icon-link text-black" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-    data-bs-details="{{ json_encode(['id' => '1', 'category' => 'address']) }}">
-    <h1>Address</h1>
-</a>
