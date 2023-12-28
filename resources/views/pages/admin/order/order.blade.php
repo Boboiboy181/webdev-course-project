b5d61c634d5c51cfc92f07ff9a9e9ddee65dbd77
Bao Chau <chauchau0082@gmail.com>
2023-12-28 20:16:46 +0700

    format code for order view

--- resources/views/pages/admin/order/order.blade.php
+++ resources/views/pages/admin/order/order.blade.php
@@ -7,7 +7,7 @@
 @endsection
 
 @section('contents')
-    @if(session()->has('success'))
+    @if (session()->has('success'))
         <div class="alert alert-success alert-dismissible mb-4" role="alert">
             <strong>Success!</strong>
             <span>{{ session('success') }}</span>
@@ -22,35 +22,24 @@
                 <p class="m-0 font-weight-bold text-primary">Order Data</p>
             </div>
             <div class="card-body">
-                <x-table
-                    :field1="'Order Code'"
-                    :field2="'Customer name'"
-                    :field3="'Total Price'"
-                    :field4="'Order Status'"
-                    :field5="'Payment Status'"
-                >
+                <x-table :field1="'Order Code'" :field2="'Customer name'" :field3="'Total Price'" :field4="'Order Status'" :field5="'Payment Status'">
                     @foreach ($orders as $order)
-                        <x-table-row
-                            :data1="$order->order_code"
-                            :data2="$order->customer['name']"
-                            :data3="$order->total_price"
-                            :data4="$order->status"
-                            :data5="$order->payment_status"
-                            :detailUrl="route('admin.order.detail', ['id' => $order->_id])"
-                        />
+                        <x-table-row :data1="$order->order_code" :data2="$order->customer['name']" :data3="$order->total_price" :data4="$order->status"
+                            :data5="$order->payment_status" :detailUrl="route('admin.order.detail', ['id' => $order->_id])" />
                     @endforeach
                 </x-table>
             </div>
         </div>
     </div>
-
 @endsection
 
 @section('custome-scripts')
     <script
-        src="{{ asset('https://startbootstrap.github.io/startbootstrap-sb-admin-2/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+        src="{{ asset('https://startbootstrap.github.io/startbootstrap-sb-admin-2/vendor/datatables/jquery.dataTables.min.js') }}">
+    </script>
     <script
-        src="{{ asset('https://startbootstrap.github.io/startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
-    <script
-        src="{{ asset('https://startbootstrap.github.io/startbootstrap-sb-admin-2/js/demo/datatables-demo.js') }}"></script>
+        src="{{ asset('https://startbootstrap.github.io/startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4.min.js') }}">
+    </script>
+    <script src="{{ asset('https://startbootstrap.github.io/startbootstrap-sb-admin-2/js/demo/datatables-demo.js') }}">
+    </script>
 @endsection
